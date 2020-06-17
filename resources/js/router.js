4054641de7c982069bee5a9c3cbbfa699fed2fc0
Soleman <manksoleman@gmail.com>
2020-06-17 23:44:01 +0700

    route-vue

---
+++ resources/js/router.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+const routes = [
+	{
+		path: '/my-new-vue-route',
+		
+	},
+
+
+]
+
+
+
+
+export default new Router({
+	mode: 'history',
+	routes
+})
