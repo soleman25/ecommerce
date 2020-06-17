8a4e41548f38e7d25052856de4d5d8833680cc81
Soleman <manksoleman@gmail.com>
2020-06-17 22:40:09 +0700

    upcoming page vue

---
+++ resources/js/components/mainapp.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="jumbotron jumbotron-fluid">
+	  <div class="container">
+	    <h1 class="display-4">Comming Son!</h1>
+	    <p class="lead">Up Comming Pages</p>
+	  </div>
+	</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
